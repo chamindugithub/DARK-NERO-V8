c332a8d779af9519f5eac3d27b1649d6bdf6e61a
chamindu git <110526143+chamindugithub@users.noreply.github.com>
2023-07-01 08:49:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,15 +20,15 @@ global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'no' //inbox block
 global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
+global.owner = ['0728833484'] //ur owner number
 global.ownername = "kaveesha" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "YkIzNnRZaWM=" //session Id
-global.ownernomer = "94786802371" //ur number
+global.session = "Q2VLdzB5V2U=" //session Id
+global.ownernomer = "94728833484" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94786802371'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
